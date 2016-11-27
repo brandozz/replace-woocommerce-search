158c6e36dc9603278de579cf692b22bc91632816
brandozz <bmc1416@gmail.com>
2016-11-27 09:57:43 -0600

    Removed form and replaced with function
    Removed the search form and replaced with the get_search_form Wordpress
    function

--- replace-woo-product-search.php
+++ replace-woo-product-search.php
@@ -13,10 +13,7 @@
    /* target filter get_product_search_form called in wc-template-functions.php */
     if (!function_exists('replace_woo_search')) {
       function replace_woo_search() {
-        echo '<form id="searchform" method="get" action="' . esc_url(home_url('/')) . '"><div>';
-        echo '<input type="text" name="s" id="s" size="15" placeholder="Search" /><br />';
-        echo '<input type="submit" value="Search" /></div>';
-        echo '</form>';
+        get_search_form();
       }
       add_filter('get_product_search_form','replace_woo_search');
     }
